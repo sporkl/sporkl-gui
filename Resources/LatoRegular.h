c67cdceaefed4183f4f64e5ec73af2563349149d
sporklpony <comradesparklepony@gmail.com>
2022-01-20 14:20:02 -0500

    add dark mode

---
+++ Resources/LatoRegular.h
@@ -0,0 +1,10 @@
+/* (Auto-generated binary data file). */
+
+#pragma once
+
+namespace LatoRegular
+{
+    extern const char*  latoregular_ttf;
+    const int           latoregular_ttfSize = 657212;
+
+}
